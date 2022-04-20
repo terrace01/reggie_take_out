c8a0b99dc1d7f2b2c809fbed8aec445c7420781f
Zhenyu Zhao <concact@zhaozhenyu.cn>
2022-04-20 22:37:17 +0800

    add: pom.xml,application and WebMvcConfig

---
+++ src/main/java/cn/luxun/reggie/ReggieApplication.java
@@ -0,0 +1,18 @@
+package cn.luxun.reggie;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReggieApplication.class, args);
+
+		log.info("Reggie started 🚀");
+	}
+
+}
